e19014c64f06aba572dadea38600f09a35ca6269
Tal Liron <tal.liron@gmail.com>
2010-04-09 06:59:36 +0000

    * Add JRuby * Custom adapter for JRuby (first commit)
    git-svn-id: https://scripturian.googlecode.com/svn/trunk@129 03550cf4-9855-cbac-b17e-ef196385c0aa

--- modules/com.threecrickets.scripturian/src/com/threecrickets/scripturian/adapter/JRubyAdapter.java
+++ modules/com.threecrickets.scripturian/src/com/threecrickets/scripturian/adapter/JRubyAdapter.java
@@ -11,50 +11,37 @@
 
 package com.threecrickets.scripturian.adapter;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-import javax.script.ScriptContext;
-import javax.script.ScriptEngine;
-import javax.script.ScriptException;
+import org.jruby.Ruby;
+import org.jruby.ast.Node;
+import org.jruby.ast.executable.Script;
+import org.jruby.embed.EvalFailedException;
+import org.jruby.embed.io.WriterOutputStream;
+import org.jruby.javasupport.JavaEmbedUtils;
 
 import com.threecrickets.scripturian.Executable;
 import com.threecrickets.scripturian.ExecutionContext;
 import com.threecrickets.scripturian.LanguageAdapter;
+import com.threecrickets.scripturian.Scriptlet;
+import com.threecrickets.scripturian.exception.CompilationException;
+import com.threecrickets.scripturian.exception.ExecutableInitializationException;
+import com.threecrickets.scripturian.exception.ExecutionException;
 import com.threecrickets.scripturian.exception.LanguageInitializationException;
 
 /**
  * An {@link LanguageAdapter} that supports the Ruby scripting language as
  * implemented by <a href="http://jruby.codehaus.org/">JRuby</a>.
- * <p>
- * Note that JRuby internally embeds each script in a "main" object, so that
- * methods defined therein cannot be accessible to us after the script runs,
- * unless they are explicitly stored in global variables. For this reason, in
- * order to make your methods invocable, they must be stored as closures in
- * global variables of the same name as the entry point. As so:
- * 
- * <pre>
- *  def myentry value
- *  	print value*3
- *  end
- *  $myentry = method :myentry
- * </pre>
- * 
- * Or even store raw lambdas:
- * 
- * <pre>
- *  $myentry = lambda do |value|
- *  	print value*3
- *  end
- * </pre>
  * 
  * @author Tal Liron
  */
-@ScriptEngines(
-{
-	"jruby", "ruby"
-})
-public class JRubyAdapter extends Jsr223LanguageAdapter
+public class JRubyAdapter implements LanguageAdapter
 {
 	//
 	// Construction
@@ -62,141 +49,152 @@ public class JRubyAdapter extends Jsr223LanguageAdapter
 
 	public JRubyAdapter() throws LanguageInitializationException
 	{
-		try
-		{
-			raiseExceptionClass = getClass().getClassLoader().loadClass( "org.jruby.exceptions.RaiseException" );
-			raiseExceptionGetExceptionMethod = raiseExceptionClass.getMethod( "getException" );
-			nativeExceptionClass = getClass().getClassLoader().loadClass( "org.jruby.NativeException" );
-			nativeExceptionGetCauseMethod = nativeExceptionClass.getMethod( "getCause" );
-		}
-		catch( ClassNotFoundException x )
-		{
-			throw new LanguageInitializationException( getClass(), x );
-		}
-		catch( SecurityException x )
-		{
-			throw new LanguageInitializationException( getClass(), x );
-		}
-		catch( NoSuchMethodException x )
-		{
-			throw new LanguageInitializationException( getClass(), x );
-		}
+		attributes.put( NAME, "JRuby" );
+		attributes.put( VERSION, "?" );
+		attributes.put( LANGUAGE_NAME, "Ruby" );
+		attributes.put( LANGUAGE_VERSION, "?" );
+		attributes.put( EXTENSIONS, Arrays.asList( "rb" ) );
+		attributes.put( DEFAULT_EXTENSION, "rb" );
+		attributes.put( TAGS, Arrays.asList( "ruby", "jruby" ) );
+		attributes.put( DEFAULT_TAG, "ruby" );
 	}
 
 	//
-	// ScriptletHelper
+	// LanguageAdapter
 	//
 
-	/*@Override
-	public boolean isCompilable()
+	public Map<String, Object> getAttributes()
 	{
-		return false;
-	}*/
-
-	@Override
-	public void beforeCall( ScriptEngine scriptEngine, ExecutionContext executionContext )
-	{
-		ScriptContext scriptContext = getScriptContext( executionContext );
-		scriptEngine.setContext( scriptContext );
-
-		// Move global vars to instance vars
-		StringBuilder r = new StringBuilder();
-		for( String var : executionContext.getExposedVariables().keySet() )
-		{
-			r.append( '@' );
-			r.append( var );
-			r.append( "=$" );
-			r.append( var );
-			r.append( ';' );
-		}
-
-		try
-		{
-			scriptEngine.eval( r.toString(), scriptContext );
-		}
-		catch( ScriptException x )
-		{
-		}
+		return attributes;
 	}
 
-	@Override
-	public String getTextAsProgram( Executable document, ScriptEngine scriptEngine, String content )
+	public boolean isThreadSafe()
 	{
-		content = content.replaceAll( "\\n", "\\\\n" );
-		content = content.replaceAll( "\\\"", "\\\\\"" );
-		return "print(\"" + content + "\");";
+		return true;
 	}
 
-	@Override
-	public String getExpressionAsProgram( Executable document, ScriptEngine scriptEngine, String content )
+	public Lock getLock()
 	{
-		return "print(" + content + ");";
+		return lock;
 	}
 
-	@Override
-	public String getExpressionAsInclude( Executable document, ScriptEngine scriptEngine, String content )
+	public String getCodeForLiteralOutput( String literal, Executable executable ) throws ExecutableInitializationException
 	{
-		// return "require $" + document.getDocumentVariableName() +
-		// ".container.source.basePath.toString + '/' + " + content + ";";
-		return "$" + document.getExposedExecutableName() + ".container.include_document(" + content + ");";
+		literal = literal.replaceAll( "\\n", "\\\\n" );
+		literal = literal.replaceAll( "\\\"", "\\\\\"" );
+		return "print(\"" + literal + "\");";
 	}
 
-	@Override
-	public String getInvocationAsProgram( Executable document, ScriptEngine scriptEngine, String content )
+	public String getCodeForExpressionOutput( String expression, Executable executable ) throws ExecutableInitializationException
 	{
-		/*
-		 * String version = scriptEngine.getFactory().getEngineVersion();
-		 * String[] split = version.split( "\\." ); int major = split.length >=
-		 * 1 ? Integer.parseInt( split[0] ) : 0; int minor = split.length >= 2 ?
-		 * Integer.parseInt( split[1] ) : 0; int revision = split.length >= 3 ?
-		 * Integer.parseInt( split[2] ) : 0;
-		 */
+		return "print(" + expression + ");";
+	}
 
+	public String getCodeForExpressionInclude( String expression, Executable executable ) throws ExecutableInitializationException
+	{
+		return "$" + executable.getExposedExecutableName() + ".container.include_document(" + expression + ");";
+	}
+
+	public Throwable getCauseOrExecutionException( String executableName, Throwable throwable )
+	{
 		return null;
-		// return content + "();";
-		// else
-		// return "$" + content + ".call;";
 	}
 
-	@Override
-	public Throwable getCauseOrExecutionException( String documentName, Throwable throwable )
+	public Scriptlet createScriptlet( String code, Executable executable ) throws ExecutableInitializationException
 	{
-		if( raiseExceptionClass.isInstance( throwable ) )
-		{
-			try
-			{
-				Object rubyException = raiseExceptionGetExceptionMethod.invoke( throwable );
-				if( nativeExceptionClass.isInstance( rubyException ) )
-					return (Throwable) nativeExceptionGetCauseMethod.invoke( rubyException );
+		return new JRubyScriptlet( code );
+	}
 
-				// Wish there were a way to get line numbers from
-				// RubyException!
-			}
-			catch( IllegalArgumentException x )
-			{
-				x.printStackTrace();
-			}
-			catch( IllegalAccessException x )
-			{
-				x.printStackTrace();
-			}
-			catch( InvocationTargetException x )
-			{
-				x.printStackTrace();
-			}
-		}
-
-		return null;
+	public Object invoke( String method, Executable executable, ExecutionContext executionContext ) throws NoSuchMethodException, ExecutableInitializationException, ExecutionException
+	{
+		Ruby ruby = getRubyRuntime( executionContext );
+		Object r = JavaEmbedUtils.rubyToJava( ruby.getTopSelf().callMethod( ruby.getCurrentContext(), method ) );
+		// System.out.println( r );
+		return r;
 	}
 
 	// //////////////////////////////////////////////////////////////////////////
 	// Private
 
-	private final Class<?> raiseExceptionClass;
+	private static final String RUBY_RUNTIME = "jruby.rubyRuntime";
 
-	private final Method raiseExceptionGetExceptionMethod;
+	private final ConcurrentMap<String, Object> attributes = new ConcurrentHashMap<String, Object>();
 
-	private final Class<?> nativeExceptionClass;
+	private final ReentrantLock lock = new ReentrantLock();
 
-	private final Method nativeExceptionGetCauseMethod;
+	private final Ruby compilerRuntime = Ruby.newInstance();
+
+	private Ruby getRubyRuntime( ExecutionContext executionContext )
+	{
+		Ruby ruby;
+		ruby = (Ruby) executionContext.getAttributes().get( RUBY_RUNTIME );
+		if( ruby == null )
+		{
+			PrintStream out = new PrintStream( new WriterOutputStream( executionContext.getWriter() ) );
+			PrintStream err = new PrintStream( new WriterOutputStream( executionContext.getErrorWriter() ) );
+			ruby = Ruby.newInstance( null, out, err );
+			executionContext.getAttributes().put( RUBY_RUNTIME, ruby );
+		}
+
+		for( Map.Entry<String, Object> entry : executionContext.getExposedVariables().entrySet() )
+			ruby.defineReadonlyVariable( "$" + entry.getKey(), JavaEmbedUtils.javaToRuby( ruby, entry.getValue() ) );
+
+		return ruby;
+	}
+
+	private class JRubyScriptlet implements Scriptlet
+	{
+		public JRubyScriptlet( String sourceCode )
+		{
+			this.sourceCode = sourceCode;
+		}
+
+		public void compile() throws CompilationException
+		{
+			// try
+			{
+				Node node = compilerRuntime.parseEval( sourceCode, "prudence", compilerRuntime.getCurrentContext().getCurrentScope(), 0 );
+				script = compilerRuntime.tryCompile( node );
+			}
+			// catch( EvalFailedException x )
+			// {
+			// throw new CompilationException( "", x.getMessage(), x );
+			// }
+		}
+
+		public Object execute( ExecutionContext executionContext ) throws ExecutableInitializationException, ExecutionException
+		{
+			Ruby ruby = getRubyRuntime( executionContext );
+
+			if( script != null )
+			{
+				return JavaEmbedUtils.rubyToJava( ruby.runScript( script ) );
+			}
+			else
+			{
+				try
+				{
+					Object r = JavaEmbedUtils.rubyToJava( ruby.evalScriptlet( sourceCode ) );
+					return r;
+				}
+				catch( EvalFailedException x )
+				{
+					throw new ExecutableInitializationException( "", x.getMessage(), x );
+				}
+			}
+			/*
+			 * catch( Throwable x ) { throw new ExecutionException(
+			 * x.getMessage(), x ); }
+			 */
+		}
+
+		public String getSourceCode()
+		{
+			return sourceCode;
+		}
+
+		private final String sourceCode;
+
+		private Script script;
+	}
 }
